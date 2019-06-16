0dc7c6251c50f3f4f1cd7a548bb6d210161eb1dd
BlueWolfDZ <46226823+BlueWolfDZ@users.noreply.github.com>
2019-06-16 01:39:41 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 client.on('ready',async () => {//join voice
 
-  client.channels.find(ch => ch.id === "560834558015045639" && ch.type === 'voice').join();
+  client.channels.find(ch => ch.id === "560214718476845056" && ch.type === 'voice').join();
 
 });
 

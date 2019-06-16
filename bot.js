c928b2361de4f4d3ffc04e6151523818a1334af7
BlueWolfDZ <46226823+BlueWolfDZ@users.noreply.github.com>
2019-06-16 01:46:11 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,10 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+
 client.on('ready',async () => {//join voice
 
   client.channels.find(ch => ch.id === "560214718476845056" && ch.type === 'voice').join();
 
 });
 
-client.login(process.env.BOT_TOKEN)
+client.login(process.env.BOT_TOKEN);

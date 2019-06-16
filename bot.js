8c883a81b3e400cd6edb9e334225bf9676bcba18
BlueWolfDZ <46226823+BlueWolfDZ@users.noreply.github.com>
2019-06-16 01:35:53 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,9 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+client.on('ready',async () => {//join voice
+
+  client.channels.find(ch => ch.id === "560834558015045639" && ch.type === 'voice').join();
+
+});
+
+client.login(process.env.BOT_TOKEN)
